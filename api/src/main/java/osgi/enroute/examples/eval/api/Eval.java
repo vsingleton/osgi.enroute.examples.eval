fc5e8f6186d170166e8cfacd9e853d3f37c7e648
vsingleton <vsingleton@gmail.com>
2016-10-20 11:54:47 -0400

    first commit

---
+++ api/src/main/java/osgi/enroute/examples/eval/api/Eval.java
@@ -0,0 +1,15 @@
+package osgi.enroute.examples.eval.api;
+
+import org.osgi.annotation.versioning.ProviderType;
+
+/**
+ * A service that evaluates an expression and returns the result
+ */
+@ProviderType
+public interface Eval {
+	/**
+	 * Evaluate an expression and return the result.
+	 */
+	double eval(String expression) throws Exception;
+}
+
